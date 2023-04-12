829b6611684b055f00f45163a4fd7eb7ec884313
daumienebi <ddsakpa@gmail.com>
2023-04-12 16:30:09 +0200

    more methods in the firestore service

--- lib/services/firestore/firestore_service.dart
+++ lib/services/firestore/firestore_service.dart
@@ -1,4 +1,5 @@
 import 'package:alice_store/models/cart_item_model.dart';
+import 'package:alice_store/models/product_model.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -14,20 +15,17 @@ class FirestoreService {
     firestore.collection('users').doc(userId).collection('cartItems');
 
     // Get all cart items for the user
-    final cartQuery = await cartReference.get();
+    final cartQuery = await cartReference.get(GetOptions(source: Source.serverAndCache));
     final cartDocs = cartQuery.docs;
-
     // Find the cart item with the matching product id
     bool itemFound = false;
     for (final cartDoc in cartDocs) {
       final cartData = cartDoc.data();
       final productId = cartData['productId'];
-
       if (productId == newCartItem.product.id) {
         // Update the quantity of the existing cart item
         final quantity = cartData['quantity'] + newCartItem.quantity;
         await cartDoc.reference.update({'quantity': quantity});
-
         itemFound = true;
         break;
       }
@@ -41,4 +39,44 @@ class FirestoreService {
     }
   }
 
+  /// Remove a whole existing cart item from the user's cart if it exists
+  Future<void> removeFromCart(String userId, CartItemModel newCartItem) async {
+    final cartReference = firestore.collection('users').doc(userId).collection('cartItems');
+    // Get all cart items for the user
+    final cartQuery = await cartReference.get(GetOptions(source: Source.serverAndCache));
+    final cartDocs = cartQuery.docs;
+    // Find the cart item with the matching product id
+    for (final cartDoc in cartDocs) {
+      final cartData = cartDoc.data();
+      final productId = cartData['productId'];
+      if (productId == newCartItem.product.id) {
+        await cartDoc.reference.delete();
+        break;
+      }
+    }
+  }
+
+  Future<List<CartItemModel>> getUserCartItems(String userId){
+    List<CartItemModel> items = [];
+
+    return Future.value(items);
+  }
+
+  Future<bool> addItemToWishList(String userId,ProductModel product){
+    List<int> items = [];
+
+    return Future.value(true);
+  }
+
+  Future<bool> removeItemFromWishList(String userId,ProductModel product){
+    //List<int> items = [];
+
+    return Future.value(true);
+  }
+
+  Future<List<int>> getWishListItems(String userId){
+    List<int> items = [];
+
+    return Future.value(items);
+  }
 }
